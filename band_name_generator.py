748e8c754ed655695151b88b4c019b7656995cd2
brandonwalsh <dassaletty@protonmail.com>
2022-12-11 03:14:20 -0600

    Day-01

---
+++ band_name_generator.py
@@ -0,0 +1,13 @@
+'''
+Band name generator
+'''
+
+
+def band_name_generator():
+    print('Welcome to the band name generator.')
+    city = input('What is the name of the city you grew up in? ')
+    pet = input('What is your pet\'s name? ')
+    print(f'Your band name could be {city} {pet}!')
+
+
+band_name_generator()
---
+++ readme.md
@@ -0,0 +1,1 @@
+# Day-01 of 100 days of Python
